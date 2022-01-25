32f2f14cf2f2c7d781e61956ecd0757742591232
binotation <68807919+binotation@users.noreply.github.com>
2022-01-25 14:25:49 +1000

    Refactor: add src/ dir

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
-const { clientId, token, guildId } = require('./config.json')
+const { clientId, token, guildId } = require('../config.json')
 
 const commands = [
     new SlashCommandBuilder().setName('test').setDescription('Test command.')
