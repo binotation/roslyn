7a20f50424d7318d269d456bc07c4b3a0f29e44b
binotation <68807919+binotation@users.noreply.github.com>
2022-01-26 13:56:42 +1000

    Removed helpers.ts

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,12 +1,12 @@
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
-import { getCommands } from './helpers'
+import commands from './commands/commands'
 
 const { clientId, token, guildId } = require('../config.json')
 
-const commands = getCommands().map(command => command.data.toJSON())
+const commandsJson = commands.map(command => command.data.toJSON())
 
 const rest = new REST().setToken(token)
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Registered application commands.'))
-    .catch(console.error)
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsJson })
+	.then(() => console.log('Registered application commands.'))
+	.catch(console.error)
